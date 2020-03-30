9292cbc134e89bdc799cf37da5bb8ab8f289421f
Alfuveam <felipe.b.t@hotmail.com>
2020-03-30 07:30:26 -0300

    Fix wrong submodule

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './global.css';
+
+import Routes from './routes'
+
+//  2 conceitos; live reload e JFX
+function App() {
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
